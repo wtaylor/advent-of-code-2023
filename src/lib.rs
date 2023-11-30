8cf81f3b6956288565ad9dda70334c4a813849ce
William Taylor <me@willtaylor.info>
2023-11-30 21:24:34 +0000

    Initial Commit

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+pub mod day01;
+
+pub fn noop(_inp: String) {}
+
+pub type DayFn = fn(String);
+
+pub fn get_day(day: u8) -> (DayFn, DayFn) {
+    return match day {
+        1 => (day01::part_1, day01::part_2),
+        _ => {
+            println!("Unknown day: {}", day);
+            return (noop, noop);
+        },
+    };
+}
