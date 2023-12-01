420ab08bf02e3bc41746e983ab6d360330c31ae3
William Taylor <me@willtaylor.info>
2023-12-01 20:16:42 +0000

    Day 1 - Part 1

--- src/day01.rs
+++ src/day01.rs
@@ -1,7 +1,13 @@
 pub fn part_1(input: String) {
+    let input = input.lines().collect::<Vec<&str>>();
+    let calibration_sum: u32 = input.iter()
+        .map(|l| l.chars().filter(|c| c.is_numeric()).collect::<Vec<char>>())
+        .map(|l| format!("{}{}", l.first().unwrap(), l.last().unwrap()).parse::<u8>().unwrap())
+        .fold(0u32, |acc, line_value| acc + u32::from(line_value));
 
+    println!("Calibration Sum: {}", calibration_sum)
 }
 
 pub fn part_2(input: String) {
-
+    
 }
