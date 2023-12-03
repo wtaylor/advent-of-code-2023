dcb8db7d9c08afe5ec21c165ec7c37672436f19c
William Taylor <me@willtaylor.info>
2023-12-03 10:48:01 +0000

    Day 1 - Part 2: Disgusting

--- src/day01.rs
+++ src/day01.rs
@@ -1,13 +1,33 @@
-pub fn part_1(input: String) {
-    let input = input.lines().collect::<Vec<&str>>();
-    let calibration_sum: u32 = input.iter()
+use std::collections::HashMap;
+
+fn calculate_calibration_sum(lines: Vec<&str>) -> u32 {
+    lines.iter()
         .map(|l| l.chars().filter(|c| c.is_numeric()).collect::<Vec<char>>())
         .map(|l| format!("{}{}", l.first().unwrap(), l.last().unwrap()).parse::<u8>().unwrap())
-        .fold(0u32, |acc, line_value| acc + u32::from(line_value));
+        .fold(0u32, |acc, line_value| acc + u32::from(line_value))
+}
 
-    println!("Calibration Sum: {}", calibration_sum)
+pub fn part_1(input: String) {
+    let input = input.lines().collect::<Vec<&str>>();
+    println!("Calibration Sum: {}", calculate_calibration_sum(input));
 }
 
 pub fn part_2(input: String) {
-    
+    let spelled_digits = Vec::from([
+        ("two", "t2wo"),
+        ("one", "on1e"),
+        ("seven", "se7en"),
+        ("nine", "ni9ne"),
+        ("three", "th3ree"),
+        ("four", "fo4ur"),
+        ("five", "fi5ve"),
+        ("six", "s6x"),
+        ("eight", "eig8ht")
+    ]);
+    let input = input.lines().collect::<Vec<&str>>();
+    let lines_with_spelled_digits_replaced = input.iter()
+        .map(|l| spelled_digits.iter().fold(l.to_string(), |replaced_string, spelled_digit| replaced_string.replace(spelled_digit.0, spelled_digit.1)))
+        .collect::<Vec<String>>();
+
+    println!("Calibration Sum: {}", calculate_calibration_sum(lines_with_spelled_digits_replaced.iter().map(AsRef::as_ref).collect()));
 }
